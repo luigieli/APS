2eb32498b69ac594811439960e18545c9f0927d7
felkng <felipersf374@gmail.com>
2025-05-26 22:23:15 -0300

    Add Endereco subtype

---
+++ src/main/java/subtypes/Endereco.java
@@ -0,0 +1,67 @@
+package subtypes;
+
+public class Endereco {
+    protected Integer numero;
+    protected String rua;
+    protected String bairro;
+    protected String cidade;
+    protected String uf;
+    protected String cep;
+
+    public Endereco(Integer numero, String rua, String cidade, String bairro, String uf, String cep) {
+        this.numero = numero;
+        this.rua = rua;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.uf = uf;
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+}
