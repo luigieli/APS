d10335b41194b66517343cf7aedda6d88fa22f89
felkng <felipersf374@gmail.com>
2025-05-31 00:17:27 -0300

    Add services classes
    What was added:
    - EventoService
    - InscricaoService
    - TrabalhoService
    - UsuarioService
    
    Co-authored-by: Marcos Marques <mdmarques70@gmail.com>
    Co-authored-by: Luigi Eliabe <luigi.eliabe.as@gmail.com>

---
+++ src/main/java/service/TrabalhoService.java
@@ -0,0 +1,31 @@
+package service;
+
+import app.Database;
+import model.Evento;
+import model.Inscricao;
+import model.Trabalho;
+import model.Usuario;
+import repository.TrabalhoRepository;
+
+import java.time.LocalDate;
+
+public class TrabalhoService {
+    private TrabalhoRepository trabalhoRepository;
+
+    public TrabalhoService() {
+        this.trabalhoRepository = Database.getInstance().getTrabalhoRepository();
+    }
+
+    public Boolean createTrabalho(Trabalho trabalho, Evento evento, Usuario usuario) throws Exception{
+        if(LocalDate.now().isAfter(evento.getPeriodoSubmissao())) throw new Exception("Período de submissão dos trabalhos já encerrou.");
+        var inscricaoOptional = usuario.getInscricoes().stream()
+                .filter(i -> i.getEvento().getIdEvento().equals(evento.getIdEvento()))
+                .findFirst();
+        if(inscricaoOptional.isEmpty()) throw new Exception("Você não está inscrito para o evento: " + evento.getNome());
+        var inscricaoFound = inscricaoOptional.get();
+        trabalho.setEvento(evento);
+        trabalho.setInscricao(inscricaoFound);
+        trabalho.setStatus("SUBMETIDO");
+        return trabalhoRepository.create(trabalho);
+    }
+}
