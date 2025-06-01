57a9f33632a3e8577928c9213cd04bc9b48e15c8
felkng <felipersf374@gmail.com>
2025-06-01 10:10:42 -0300

    Add persistence in usuario and trabalho repository
    Co-authored-by: Marcos Marques <mdmarques70@gmail.com>
    Co-authored-by: Luigi Eliabe <luigi.eliabe.as@gmail.com>

--- src/main/java/service/CertificadoService.java
+++ src/main/java/service/CertificadoService.java
@@ -7,6 +7,7 @@ import model.Trabalho;
 import model.Usuario;
 import repository.CertificadoRepository;
 import repository.TrabalhoRepository;
+import repository.UsuarioRepository;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -14,9 +15,11 @@ import java.util.UUID;
 public class CertificadoService {
     CertificadoRepository certificadoRepository;
     TrabalhoRepository trabalhoRepository;
+    UsuarioRepository usuarioRepository;
     public CertificadoService() {
         this.certificadoRepository = Database.getInstance().getCertificadoRepository();
         this.trabalhoRepository = Database.getInstance().getTrabalhoRepository();
+        this.usuarioRepository = Database.getInstance().getUsuarioRepository();
     }
 
     public String generateCertifyCode(){
@@ -24,11 +27,14 @@ public class CertificadoService {
     }
 
     public Boolean generatePresenceCertificate(Evento evento, Usuario usuario){
-        return certificadoRepository.create(new Certificado("PRESENÇA", generateCertifyCode(), evento.getDataInicio(), usuario, evento));
+        Certificado certificado = new Certificado("PRESENÇA", generateCertifyCode(), evento.getDataInicio(), usuario, evento);
+        usuario.getCertificados().add(certificado);
+        usuarioRepository.update(usuario);
+        return certificadoRepository.create(certificado);
     }
 
     public Boolean generateWorkShowCertificate(Trabalho trabalho, Evento evento, Usuario usuario){
-        var certificado = new Certificado("APRESENTAÇÃO",generateCertifyCode(),evento.getDataInicio(),usuario,evento,trabalho);
+        Certificado certificado = new Certificado("APRESENTAÇÃO",generateCertifyCode(),evento.getDataInicio(),usuario,evento,trabalho);
         trabalho.getCertificados().add(certificado);
         trabalhoRepository.update(trabalho);
         return certificadoRepository.create(certificado);
